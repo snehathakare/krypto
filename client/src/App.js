b2b76eef008778bf1b0a8dc06a2f05d16793bcba
Sneha <snhthakare@gmail.com>
2022-03-22 14:28:17 +0530

    Adds Nav links

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import { Nav, Footer, Services, Transactions, Loader } from './components';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="h-screen flex flex-col bg-gradient-to-r from-indigo-500 via-voilet-400 to-blue-200">
       <Nav />
       <Services />
       <Transactions />
       <Footer />
-      <Loader />
     </h1>
   )
 }
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>
-            <h1>Nav</h1>
+        <div className="w-full flex justify-between text-white">
+            <h1 className="pl-10 py-5 text-2xl">KRYPT</h1>
+            <ul className="flex">
+                <li className="px-10 py-5">Market</li>
+                <li className="px-10 py-5">Exchange</li>
+                <li className="px-10 py-5">Tutorial</li>
+                <li className=""><button class="m-3.5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Login
+                </button></li>
+            </ul>
         </div>
     )
 }
