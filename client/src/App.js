12ce7bddad9d5991f5e0b7a54d46405a46db41a5
Sneha <snhthakare@gmail.com>
2022-03-22 11:45:39 +0530

    Frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Nav, Footer, Services, Transactions, Loader } from './components';
 
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
+      <Nav />
+      <Services />
+      <Transactions />
+      <Footer />
+      <Loader />
     </h1>
   )
 }
