a0c55d889483513f02d6255f4da118da6671352d
Sneha <snhthakare@gmail.com>
2022-03-30 21:24:52 +0530

    Adds logic to send transaction to another account

--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -10,7 +10,7 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractAbi, signer);
 
-    console.log("test", {
+    console.log({
         provider,
         signer,
         transactionContract
@@ -20,7 +20,8 @@ const getEthereumContract = () => {
 export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState('')
     const [formData, setformData] = useState({ addressTo: '', amount: '', keyword: '', message: '' })
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
 
     const checkIfWalletIsConnected = async () => {
         if (!ethereum) return alert('Please install Metamask')
@@ -45,10 +46,40 @@ export const TransactionProvider = ({ children }) => {
 
     const sendTransaction = async () => {
         try {
-            if (!ethereum) return alert("Please install MetaMask.");
-            //add transaction logic
-            const { addressTo, amount, keyword, message } = formData
-            getEthereumContract();
+            if (ethereum) {
+                //add transaction logic
+                const { addressTo, amount, keyword, message } = formData
+                const transactionContract = getEthereumContract();
+                const parsedAmount = ethers.utils.parseEther(amount)
+                ethereum
+                    .request({
+                        method: 'eth_sendTransaction',
+                        params: [
+                            {
+                                from: currentAccount,
+                                to: addressTo,
+                                value: parsedAmount._hex,
+                                gas: '0x2710',
+                            },
+                        ],
+                    })
+
+                const transactionHash = transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword)
+
+                setIsLoading(true);
+                console.log(`Loading - ${transactionHash.hash}`);
+                await transactionHash.wait();
+                console.log(`Success - ${transactionHash.hash}`);
+                setIsLoading(false);
+
+                const transactionCount = await transactionContract.getTransactionCount();
+
+                setTransactionCount(transactionCount.toNumber());
+                window.location.reload();
+            } else {
+                alert("Please install metamask!")
+            }
+
         } catch (error) {
             console.log(error);
 
@@ -57,6 +88,7 @@ export const TransactionProvider = ({ children }) => {
     };
 
     const handleChange = (e, name) => {
+        //get data from form
         setformData((prevState) => ({ ...prevState, [name]: e.target.value }));
     }
 
